dacd272ebac081f2d16a47d68f9a3521607eb588
Aleksander Binion <aleksander.binion@gmail.com>
2023-01-24 13:53:51 -0500

    update note function

--- functions/note.sh
+++ functions/note.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
 function note {
+    readonly yesterday="$(gdate --date='yesterday' +'%Y%m%d')"
+    readonly today="$(date +'%Y%m%d')"
+
+    note_date="$today"
+    yesterday_pattern="^y.*$"
+    days_ago_pattern="^[0-9]*$"
+    get_date_pattern="^d[0-9]*$"
+    if [[ $1 =~ $yesterday_pattern ]]; then
+        note_date="$yesterday";
+    elif [[ $1 =~ $days_ago_pattern ]]; then
+        note_date=$(gdate --date="$1 days ago" +"%Y%m%d")
+    elif [[ $1 =~ $get_date_pattern ]]; then
+        note_date=$1
+        note_date="${note_date:1}"
+    fi
+
     # touch and open a note document
-    readonly file_name="$HOME/Notes/$(date +'%Y%m%d').txt"
+    readonly file_name="$HOME/Notes/$note_date.txt"
     touch "$file_name"
     open_defualt_editor "$file_name"
 }
