b4fd3c299774cc2a027db138bcbe6a4c1bbc8445
Aleksander Binion <aleksander.binion@gmail.com>
2023-03-10 15:51:40 -0500

    adding functions for managing vscode configurations

--- functions/vs_code_helpers.sh
+++ functions/vs_code_helpers.sh
@@ -1,6 +1,48 @@
 #!/bin/bash
 
-# open vscode settings file
+# check if vscode is installed
+function vscode_check_install {
+  readonly expected_dir="$HOME/Library/Application Support/Code"
+  if [ ! -d "$expected_dir" ] 
+  then
+      echo "VSCode may not be installed. Directory $expected_dir does not exist." 
+      kill -PID
+  fi
+}
+
 function vscode_open_settings {
+  vscode_check_install
   code "$HOME/Library/Application Support/Code/User/settings.json"
 }
+
+### Helpers for saving and restoring settings and keybindings
+function vscode_save_settings {
+  vscode_check_install
+  cp "$HOME/Library/Application Support/Code/User/settings.json" "$HOME/.vscode_configs"
+}
+
+function vscode_restore_settings {
+  vscode_check_install
+  cp "$HOME/.vscode_configs/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+}
+
+function vscode_save_keybindings {
+  vscode_check_install
+  cp "$HOME/Library/Application Support/Code/User/keybindings.json" "$HOME/.vscode_configs"
+}
+
+function vscode_restore_keybindings {
+  vscode_check_install
+  cp "$HOME/.vscode_configs/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
+}
+
+function vscode_save_config {
+  vscode_save_settings
+  vscode_save_keybindings
+}
+
+function vscode_restore_config {
+  vscode_restore_settings
+  vscode_restore_keybindings
+}
+###
