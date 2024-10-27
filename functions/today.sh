b99006a8ba38bcd5111dba74b1c6be7e96eefda5
Aleksander Binion <aleksander.binion@gmail.com>
2024-10-27 00:42:46 -0400

    add todayf function

--- functions/today.sh
+++ functions/today.sh
@@ -6,3 +6,17 @@ function today {
     # echo today's date
     echo "$today_date"
 }
+
+# create a file with today's date. If no extension is provided, a .txt file is created.
+function todayf {
+    leading_period="\..*"
+    if [[ -z "$1" ]]; then
+        extension="txt"
+    elif [[ $1 =~ $leading_period ]]; then
+        extension="${1:1}" 
+    else
+        extension="$1";
+    fi
+    echo "$(today).$extension" | xargs touch && echo "created: $(pwd)/$(today).$extension"
+}
+
